df007a4f9df88b60e8d3b3b8c2c544a8ec0ac204
RecursionExcursion <rloup15@gmail.com>
2022-09-07 12:40:05 -0400

    Climbing stairs and RemoveDuplicatesFromSortedList problems completed

---
+++ src/RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,35 @@
+package RemoveDuplicatesFromSortedList;
+
+public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        ListNode tempHead =
+                new ListNode(1,
+                        new ListNode(1,
+                                new ListNode(2,
+                                        new ListNode(3,
+                                                new ListNode(3)))));
+
+        tempHead = new ListNode();
+
+
+        System.out.println(deleteDuplicates(tempHead));
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode tempNode = head;
+
+        while(tempNode != null && tempNode.next != null){
+            if (tempNode.val == tempNode.next.val) {
+
+                tempNode.next = tempNode.next.next;
+            } else{
+                tempNode = tempNode.next;
+            }
+
+        }
+
+        return head;
+    }
+}
+
+
