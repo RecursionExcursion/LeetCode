374ad2e6699786bce019c6096d620f136388847f
RecursionExcursion <rloup15@gmail.com>
2022-09-15 12:03:59 -0400

    2 problems today. SingleNumber and ValidPalindrome.

---
+++ src/SingleNumber.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+
+        int[] nums = {4, 1, 2, 1, 2};
+
+        System.out.println(singleNumber2(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+
+        List<Integer> listOfDups = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    listOfDups.add(nums[i]);
+                }
+            }
+        }
+
+        for (int n : nums) {
+            if (!listOfDups.contains(n)) {
+                return n;
+            }
+        }
+        return -1;
+    }
+
+    public static int singleNumber2(int[] nums) {
+        int ans = 0;
+        for (int i : nums) {
+            ans = ans ^ i;
+        }
+        return ans;
+    }
+}
---
+++ src/ValidPalindrome.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String phrase = "A man, a plan, a canal: Panama";
+
+        System.out.println(isPalindrome(phrase));
+    }
+
+    public static boolean isPalindrome(String s) {
+
+        List<String> lettersOnly = Arrays.stream(s.split(""))
+                                         .filter(letter -> letter.matches("(?i)[a-z0-9]"))
+                                         .toList();
+
+        for (int i = 0; i < lettersOnly.size(); i++) {
+            if (!lettersOnly.get(i).equalsIgnoreCase(
+                    lettersOnly.get(lettersOnly.size() - 1 - i))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
