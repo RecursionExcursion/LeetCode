62db31d4554c2164e665e5f3c8cd86ccc57f67ba
RecursionExcursion <rloup15@gmail.com>
2022-10-31 11:01:35 -0400

    2 Problems. PowerOfFour and ReverseString was completed.

---
+++ src/ReverseString.java
@@ -0,0 +1,16 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        char[] c = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(c);
+        System.out.println(c);
+    }
+
+    public static void reverseString(char[] s) {
+        char temp;
+        for (int i = s.length - 1, j = 0; i > (s.length - 1) / 2; i--, j++) {
+            temp = s[j];
+            s[j] = s[i];
+            s[i] = temp;
+        }
+    }
+}
