9d55e882ea6c7710c54749fbbd8588377c65efcf
RecursionExcursion <rloup15@gmail.com>
2022-11-21 09:12:31 -0500

    1 problem. ArrangingCoins

---
+++ src/ArrangingCoins.java
@@ -0,0 +1,17 @@
+public class ArrangingCoins {
+    public static void main(String[] args) {
+        System.out.println(arrangeCoins(5));
+        System.out.println(arrangeCoins(3));
+        System.out.println(arrangeCoins(8));
+    }
+
+    public static int arrangeCoins(int n) {
+        int take = 1;
+        int rows = 0;
+        while (take <= n) {
+            n -= take++;
+            rows++;
+        }
+        return rows;
+    }
+}
