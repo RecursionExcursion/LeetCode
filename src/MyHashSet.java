f02322284a5ed85bd901d8e093e435095a5e143e
RecursionExcursion <rloup15@gmail.com>
2022-12-19 14:57:01 -0500

    3 problems. JumpGame, MyHashSet, RobotReturnToOrigin

---
+++ src/MyHashSet.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class MyHashSet {
+
+    private final List<Integer> setList;
+
+    public MyHashSet() {
+        setList = new ArrayList<>();
+    }
+
+    public void add(int key) {
+        if(!setList.contains(key)) setList.add(key);
+    }
+
+    public void remove(int key) {
+        if(setList.contains(key)) setList.remove((Integer) key);
+    }
+
+    public boolean contains(int key) {
+        return setList.contains(key);
+    }
+}
---
+++ src/RobotReturnToOrigin.java
@@ -0,0 +1,29 @@
+public class RobotReturnToOrigin {
+    public static void main(String[] args) {
+
+        System.out.println(judgeCircle("UD"));
+        System.out.println(judgeCircle("LL"));
+
+    }
+
+
+    private static boolean judgeCircle(String moves) {
+
+        int xAxis = 0;
+        int yAxis = 0;
+
+        char[] movements = moves.toCharArray();
+
+        for (char c : movements) {
+            switch (c) {
+                case 85 -> yAxis++;
+                case 68 -> yAxis--;
+                case 82 -> xAxis++;
+                case 76 -> xAxis--;
+            }
+        }
+
+        return xAxis == 0 && yAxis == 0;
+
+    }
+}
