829efdb031977136940a199350d6b9ed546d69e3
RecursionExcursion <rloup15@gmail.com>
2023-01-02 18:17:15 -0500

    problem. LargestOddNumberInString and MergeSimilarItems

---
+++ src/LargestOddNumberInString.java
@@ -0,0 +1,25 @@
+public class LargestOddNumberInString {
+    public static void main(String[] args) {
+        String n1 = "52";
+        String n2 = "4206";
+        String n3 = "35427";
+        String n4 = "42506";
+
+        System.out.println(largestNumber(n1));
+        System.out.println(largestNumber(n2));
+        System.out.println(largestNumber(n3));
+        System.out.println(largestNumber(n4));
+    }
+
+    private static String largestNumber(String num) {
+
+
+        int l = num.length();
+        for (int i = l - 1; i >= 0; i--) {
+            if (num.charAt(i) % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+}
