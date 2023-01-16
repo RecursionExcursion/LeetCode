ded4e2ce954159349cd268d5247a5acfe5841441
RecursionExcursion <rloup15@gmail.com>
2023-01-16 18:58:26 -0500

    1 problem, ComplementOfBase10Int

---
+++ src/ComplementOfBase10Int.java
@@ -0,0 +1,22 @@
+public class ComplementOfBase10Int {
+
+    public static void main(String[] args) {
+        System.out.println(bitwiseComplement(5));
+        System.out.println(bitwiseComplement(7));
+        System.out.println(bitwiseComplement(10));
+
+    }
+
+    public static int bitwiseComplement(int n) {
+        char[] binaryValue = Integer.toBinaryString(n).toCharArray();
+
+        for (int i = 0; i < binaryValue.length; i++){
+            binaryValue[i] = (binaryValue[i] == '0' ? '1' : '0');
+        }
+
+      return Integer.parseInt(String.valueOf(binaryValue),2);
+
+
+
+    }
+}
