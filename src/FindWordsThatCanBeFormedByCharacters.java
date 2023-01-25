2cc483006f6ae42c47bfa48140700f943d9b53fc
RecursionExcursion <rloup15@gmail.com>
2023-01-24 21:28:38 -0500

    1 problem, FindWordsThatCanBeFormedByCharacters

---
+++ src/FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,64 @@
+public class FindWordsThatCanBeFormedByCharacters {
+
+    public static void main(String[] args) {
+
+        String[] words = {"cat", "bt", "hat", "tree"};
+        String[] words2 = {"hello", "world", "leetcode", "tree"};
+
+
+        System.out.println(countCharacters(words, "atach"));
+        System.out.println(countCharacters(words2, "welldonehoneyr"));
+
+    }
+
+    public static int countCharacters(String[] words, String chars) {
+
+        int length = 0;
+
+        for (String word : words) {
+            char[] tempCharArr = chars.toCharArray();
+            char[] wordChars = word.toCharArray();
+            boolean flag = false;
+            for (char c : wordChars) {
+                flag = false;
+                for (int i = 0; i < tempCharArr.length; i++) {
+                    if (tempCharArr[i] != 0 && c == tempCharArr[i]) {
+                        tempCharArr[i] = 0;
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag) break;
+            }
+            if (flag) length += word.length();
+        }
+        return length;
+    }
+
+//    public static int countCharacters(String[] words, String chars) {
+//
+//        int length = 0;
+//
+//        List<Character> charList = new java.util.ArrayList<>(IntStream.range(0, chars.length())
+//                                                                      .mapToObj(chars::charAt)
+//                                                                      .toList());
+//
+//        for (String word : words) {
+//
+//            List<Character> tempList = new ArrayList<>(charList);
+//            boolean flag = true;
+//            char[] wordChars = word.toCharArray();
+//
+//            for (char c : wordChars) {
+//                boolean isPresent = tempList.remove(Character.valueOf(c));
+//                if (!isPresent) {
+//                    flag = false;
+//                    break;
+//                }
+//            }
+//            if (flag) length += word.length();
+//        }
+//
+//        return length;
+//    }
+}
