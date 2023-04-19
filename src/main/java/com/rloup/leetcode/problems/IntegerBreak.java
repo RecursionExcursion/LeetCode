c3e9c34779c7b34eb4abd2f70e8b887639ee73bb
RecursionExcursion <rloup15@gmail.com>
2023-04-19 18:03:56 -0400

    1 problem, IntegerBreak

--- src/main/java/com/rloup/leetcode/problems/IntegerBreak.java
+++ src/main/java/com/rloup/leetcode/problems/IntegerBreak.java
@@ -1,11 +1,11 @@
 package com.rloup.leetcode.problems;
 
+import java.util.stream.IntStream;
+
 public class IntegerBreak {
 
-    public int integerBreak(int n) {
-
+    public int integerBreak2(int n) {
         double highestResult = -1;
-
         for (int i = n / 2; i <= n * 1.5; i++) {
             for (int j = 1; j < i; j++) {
                 if (i % j == 0) {
@@ -18,5 +18,20 @@ public class IntegerBreak {
             }
         }
         return (int) highestResult;
+    }
+
+    public int integerBreak(int n) {
+        return IntStream.range(n / 2, (int) (n * 1.5))
+                        .mapToObj(i -> IntStream.range(1, i)
+                                                .filter(j -> i % j == 0)
+                                                .mapToDouble(j -> {
+                                                    double pow = Math.pow(j, ((double) i / j - 1));
+                                                    int c = j + n - i, lastMult = c == 0 ? 1 : c;
+                                                    return lastMult > 0 ? pow * lastMult : pow / lastMult;
+                                                }))
+                        .map(s -> (int) s.max().orElse(0))
+                        .max(Double::compare)
+                        .orElse(0);
+
     }
 }
