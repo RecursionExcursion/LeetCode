0c763975137df7a5b3a3eb7f8d80f3f005f85d83
RecursionExcursion <rloup15@gmail.com>
2023-04-19 11:44:10 -0400

    1 problem, BullsAndCows

--- src/main/java/com/rloup/leetcode/problems/BullsAndCows.java
+++ src/main/java/com/rloup/leetcode/problems/BullsAndCows.java
@@ -9,25 +9,13 @@ public class BullsAndCows {
 
         int bulls = 0;
         for (int i = 0; i < secretChars.length; i++) {
-            int numericValue = Character.getNumericValue(secretChars[i]);
-            occurMap[numericValue]++;
-            if (secretChars[i] == guessChars[i]) {
-                bulls++;
-            }
+            occurMap[Character.getNumericValue(secretChars[i])]++;
+            if (secretChars[i] == guessChars[i]) bulls++;
         }
-
-        for (char c : guessChars) {
-            int numericValue = Character.getNumericValue(c);
-            if (occurMap[numericValue] > 0) {
-                occurMap[numericValue]--;
-            }
-        }
-        int notCows = 0;
-        for (int i : occurMap) {
-            notCows += i;
-        }
-        int cows = guessChars.length - bulls - notCows;
-
+        for (char c : guessChars) occurMap[Character.getNumericValue(c)]--;
+        int cows = 0;
+        for (int i : occurMap) if (i > 0) cows += i;
+        cows = guessChars.length - bulls - cows;
         return String.format("%sA%sB", bulls, cows);
     }
 }
